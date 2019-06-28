19da0a7a29ca0c085ce57b12d9c4b68a0b9f0c3c
CQD <cqd@hiigara.net>
2019-06-28 12:50:10 +0800

    Implement element query / creation

--- src/iquery.js
+++ src/iquery.js
@@ -1,6 +1,94 @@
 (function(){
-var iQuery = function(){
+var selectElements = function(selector, context){
+    if (!selector) return [];
+    context = context || document;
+
+    var elems = [];
+    if ( typeof selector === "string") {
+        // Create an element by tag name
+        var gtPos = selector.indexOf(">");
+        if (selector[0] === "<" && gtPos >= 2) {
+            elems = [ document.createElement(selector.substring(1, gtPos)) ];
+        }
+
+        // Select elements with query selector
+        else if (context.querySelectorAll) {
+            elems = context.querySelectorAll(selector);
+        }
+    }
+
+    // $(DOMElement)
+    else if (selector.nodeType) {
+        elems = [selector];
+    }
+
+    // $([DOMElement, DOMElement, DOMElement]), for internal use only
+    else if (
+        typeof selector.length === "number" &&
+        (selector.length === 0 || ( selector.length - 1 ) in selector)
+    ) {
+        var i = 0;
+        for (; i < selector.length; i++) {
+            if (selector[i].nodeType && containElement(context, selector[i])) {
+                elems.push(selector[i]);
+            }
+        }
+    }
+
+    return elems;
 }
 
-window.iQuery = window.$ = iQuery;
+var containElement = function(tree, node)
+{
+    if (tree === document) return true;
+
+    while (node = node.parentNode) {
+        if (tree === node) {
+            return true;
+        }
+    }
+    return false;
+}
+
+var iQuery = function(selector, context){
+
+    var elems = selectElements(selector, context);
+    var i = 0;
+
+    for (; i < elems.length; i++) {
+        this[i] = elems[i];
+    }
+    this.length = elems.length;
+
+    return this;
+}
+
+iQuery.prototype = {
+    iQuery: true,
+    length: 0,
+    eq: function(idx){
+        return new iQuery(this[idx])
+    },
+    find: function(selector){
+        // TODO need speed up...?
+        var i = 0, j = 0, elems = [], subElems;
+        for (; i < this.length; i++) {
+            subElems = selectElements(selector, this[i]);
+            for (j = 0; j < subElems.length; j++) {
+                elems.push(subElems[j])
+            }
+        }
+
+        elems.filter(function(v,idx,self){
+            return self.indexOf(v) === idx;
+        });
+
+        return new iQuery(elems);
+    }
+};
+
+window.iQuery = window.$ = function(selector, context){
+    return new iQuery(selector, context);
+};
+
 })()
--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,55 @@
-QUnit.test( "Basic Test", function( assert ) {
-  assert.equal(typeof window.$, "function", 'window.$ should be a function' );
-  assert.equal(typeof window.iQuery, "function", 'window.iQuery should be a function' );
+QUnit.test("Basic Test", function(assert) {
+    assert.equal(typeof window.$, "function", 'window.$ should be a function');
+    assert.equal(typeof window.iQuery, "function", 'window.iQuery should be a function');
+});
+
+
+QUnit.test("Select elements", function(assert) {
+    var $e;
+
+    // Selector is `falsy`
+    [undefined, '', false].forEach(function(v){
+        $e = $(v);
+        assert.equal(0, $e.length, 'Falsy selector');
+        assert.equal(undefined, $e[0], 'Falsy selector');
+    });
+
+    // single element select
+    $e = $('body');
+    assert.equal($e.length, 1, 'Single element');
+    assert.equal($e[0].tagName, 'BODY', 'Single element');
+
+    // multiple element select
+    $e = $('div#iqueryPlayGround ul');
+    assert.equal($e.length, 2, 'Multiple elements');
+    assert.equal($e[0].tagName, 'UL', 'Multiple elements');
+
+    // find sub elements
+    $e = $('div#iqueryPlayGround ul').find('li');
+    assert.equal($e.length, 6, 'Find');
+    assert.equal($e[0].tagName, 'LI', 'Find');
+    assert.equal($e[1].id, 'a2', 'Find');
+    assert.equal($e[5].id, 'b3', 'Find');
+
+    $e = $('div#iqueryPlayGround ul#b').find('li');
+    assert.equal($e.length, 3, 'Find');
+    assert.equal($e[0].tagName, 'LI', 'Find');
+    assert.equal($e[2].id, 'b3', 'Find');
+
+    // select with context
+    $e = $('li', document.getElementById('a'));
+    assert.equal($e.length, 3, 'Query with context');
+    assert.equal($e[0].tagName, 'LI', 'Query with context');
+    assert.equal($e[2].id, 'a3', 'Query with context');
+
+});
+
+QUnit.test("Create element", function(assert) {
+    $e = $('<div>');
+    assert.equal($e.length, 1, 'Create div');
+    assert.equal($e[0].tagName, 'DIV', 'Create div');
+
+    $e = $('<div></div>');
+    assert.equal($e.length, 1, 'Create div');
+    assert.equal($e[0].tagName, 'DIV', 'Create div');
 });
