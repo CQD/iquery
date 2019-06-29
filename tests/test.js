3535b55dd87174e1b7227048fa2dd7d2cf1fa911
CQD <cqd@hiigara.net>
2019-06-29 22:09:01 +0800

    Ask QUnit always check for global contamination

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,5 @@
+QUnit.config.noglobals = true;
+
 QUnit.test("Basic Test", function(assert) {
     assert.equal(typeof window.$, "function", 'window.$ should be a function');
     assert.equal(typeof window.iQuery, "function", 'window.iQuery should be a function');
